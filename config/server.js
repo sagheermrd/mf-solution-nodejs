cbe0024f14a2555912a552bf458728428f921208
Sagheer <>
2020-04-12 16:02:47 +0100

    add enpoint to get repeated words by providing input as query string

--- config/server.js
+++ config/server.js
@@ -1,9 +1,14 @@
-import express from "express";
+import express from 'express'
+import {RepeatedWords} from '../src/utility';
 const server = express();
 
 server.use(express.json());
+
+server.get('/',(req,res) => res.send('Duplicate words finder, please use GET: /api/dupicates?input=abc'))
 server.get('/api/duplicates',(req,res)=>{
-    res.json([]);
+    const {input} = req.query;
+    var result= RepeatedWords(input);
+    res.json(result);
 })
 export default server;
 
