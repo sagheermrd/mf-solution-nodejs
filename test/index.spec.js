36eca92ccbb68d379b4743c5f2ac7ae7c437fbda
Sagheer <>
2020-04-12 15:34:16 +0100

    added test case to find top three repeated words

--- test/index.spec.js
+++ test/index.spec.js
@@ -19,3 +19,22 @@ describe("Can find duplicate words ", () => {
     })
   })  
 })
+
+describe("Get repeated words from paragraph", () => {
+  it("should return repeated words array", () => {
+    
+    let input = `In a village of La Mancha, the name of which I have no desire to call to
+    mind, there lived not long since one of those gentlemen that keep a lance
+    in the lance-rack, an old buckler, a lean hack, and a greyhound for
+    coursing.An olla of rather more beef than mutton, a salad on most
+    nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
+    on Sundays, made away with three - quarters of his income`;
+    
+    const words = RepeatedWords(input);
+    expect(words.length).to.equal(3);
+    expect(words[0],`first word: ${words[0]}`).to.equal("a");
+    expect(words[1],`second word: ${words[1]}`).to.equal("of");
+    expect(words[2], `Third word: ${words[2]}`).to.equal("on");
+    console.log(`repeated words are: ${words[0]}, ${words[1]}, ${words[2]}`)
+  })
+})
